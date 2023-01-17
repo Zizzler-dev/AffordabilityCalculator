3a1565930b5f6eef145fd6dff0a064aa1bde883b
Zizzler-dev <113946881+Zizzler-dev@users.noreply.github.com>
2023-01-17 13:22:37 -0600

    Update AffordabilityScript.py

--- AffordabilityScript.py
+++ AffordabilityScript.py
@@ -23,23 +23,25 @@ def convert_df(df):
 
 if census is not None:
 
-    choice = st.radio('Choose One', ['Age Adjusted', 'Flat'])
+    choice = st.radio('Choose One', ['Age Adjusted', 'Flat', 'Custom'])
 
     percent_increase_df = pd.DataFrame()
     censusdf = pd.read_csv(census)
-    Premium_Data = pd.read_csv('Premium Data.csv')
+    Premium_Data = pd.read_csv('LCSPP_21_single.csv')
     Zip_to_County = pd.read_csv('Zip To County.csv')
     Age_Curve = pd.read_csv('Age Curve.csv')
 
     affordable = 0
     unaffordable = pd.DataFrame()
 
+    st.write(censusdf)
+
     bosscontribution = st.number_input('Input Employer Contribution (21/Single): ')
 
     if bosscontribution > 0:
         Zip_to_County = Zip_to_County.drop_duplicates(subset = ['Zip Code'], ignore_index= True)
 
-        join = pd.merge(Zip_to_County[['Zip Code', 'county', 'State Key']], Premium_Data[['county','LCSPP']], on = 'county', how = 'inner')
+        join = pd.merge(Zip_to_County[['Zip Code', 'FIPS', 'State Key']], Premium_Data[['FIPS','LCSPP21']], on = 'FIPS', how = 'inner')
         #st.write(join)
         join = pd.merge(censusdf[['First Name','Last Name', 'DOB', 'Zip Code', 'Salary']], join, on = 'Zip Code', how = 'inner')
         #st.write(join.reset_index())
@@ -49,12 +51,13 @@ if census is not None:
         for i in join.index:
             age = calculateAge(join['DOB'][i])
             join['State Key'][i] = join['State Key'][i]+str(age)
+        #st.write(join)
         
         join = pd.merge(join, Age_Curve[['State Key', 'Value']], on = 'State Key', how = 'inner')
         #st.write(join)
         #join = join.drop_duplicates(subset=['First Name', 'Last Name', 'DOB', 'Zip Code', 'Salary', 'county', 'State Key', 'rate' ], ignore_index=True)
 
-        st.write(join)
+        #st.write(join)
 
 
         join['Increase'] = " "
@@ -70,14 +73,39 @@ if census is not None:
 
             if(choice == 'Age Adjusted'):
                 employercontribution = bosscontribution * join['Value'][i]
-            else:
+            elif(choice == 'Flat'):
                 employercontribution = bosscontribution
+            elif(choice == 'Custom'):
+                age = calculateAge(join['DOB'][i])
+                if(age <= 44):
+                    employercontribution = 262
+                elif(age >= 45 and age <47):
+                    employercontribution = 273
+                elif(age >= 47 and age <50):
+                    employercontribution = 308
+                elif(age == 50):
+                    employercontribution = 326
+                elif(age > 50 and age < 54):
+                    employercontribution = 382
+                elif(age == 54):
+                    employercontribution = 391
+                elif(age >=55 and age < 58):
+                    employercontribution = 503
+                elif(age >= 58 and age < 60):
+                    employercontribution = 638
+                elif(age >= 60 and age < 63):
+                    employercontribution = 680
+                elif(age >= 63 and age <65):
+                    employercontribution = 700
+                elif(age >= 65):
+                    employercontribution = 786
 
-            premium = round (join['LCSPP'][i] * join['Value'][i], 3)
+
+            premium = round (join['LCSPP21'][i] * join['Value'][i], 3)
 
             Employee_Contribution = premium - employercontribution
  
-            Affordability_Threshold = (int(join['Salary'][i]) / 12) * 0.0961
+            Affordability_Threshold = (int(join['Salary'][i]) / 12) * 0.0912
 
             
             join['Increase'][i] = (premium - (Affordability_Threshold + employercontribution)) / employercontribution 
@@ -91,7 +119,7 @@ if census is not None:
 
         join = join.sort_values(by = 'Increase')
         join = join.reset_index()
-        #st.write(join)
+        st.write(join)
         
 
         employercontribution = bosscontribution
@@ -122,7 +150,7 @@ if census is not None:
         else:
 
             increase_index = round((len(join.index)) * 0.95)
-            percent_increase = round(join['Increase'][increase_index], 4)
+            percent_increase = round(join['Increase'][increase_index-1], 4)
 
             st.write('Your contribution is considered Unaffordable with ',final_affordability,'% of your employees having an affordable contribution. An increase of ', percent_increase * 100, '% is required to have an 95% affordable contribution. This equates to a contribution of ', round(bosscontribution+(bosscontribution * percent_increase)), 'for a single, 21 year old employee.')
             st.download_button(
